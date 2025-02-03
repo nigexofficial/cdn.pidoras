e0a6bd0fdb135dfaf85260bfc75114f07c4a1256
SonicPika <koteykaandchernishka@gmail.com>
2025-02-03 10:20:04 +0100

    added fix

--- assets/scripts/ads.js
+++ assets/scripts/ads.js
@@ -1,17 +1,22 @@
-const images = [
-  { src: 'https://cdn.pidoras.xyz/assets/images/advertisements/ad1.png', link: 'https://nigex.xyz' },
-  { src: 'https://cdn.pidoras.xyz/assets/images/advertisements/ad2.jpeg', link: 'https://nigex.xyz' },
-  { src: 'https://cdn.pidoras.xyz/assets/images/advertisements/ad3.jpeg', link: 'https://nigex.xyz' },
-  { src: 'https://cdn.pidoras.xyz/assets/images/advertisements/ad4.jpg', link: 'https://nigex.xyz' },
-  { src: 'https://cdn.pidoras.xyz/assets/images/advertisements/ad5.jpeg', link: 'https://nigex.xyz' },
-  { src: 'https://cdn.pidoras.xyz/assets/images/advertisements/ad6.jpg', link: 'https://nigex.xyz' },
-  { src: 'https://cdn.pidoras.xyz/assets/images/advertisements/ad7.png', link: 'https://nigex.xyz/vklog' }
-];
-        
-const randomIndex = Math.floor(Math.random() * images.length);
-const selectedImage = images[randomIndex];
+document.addEventListener("DOMContentLoaded", function () {
+    const images = [
+        { src: 'https://cdn.pidoras.xyz/assets/images/advertisements/ad1.png', link: 'https://nigex.xyz' },
+        { src: 'https://cdn.pidoras.xyz/assets/images/advertisements/ad2.jpeg', link: 'https://nigex.xyz' },
+        { src: 'https://cdn.pidoras.xyz/assets/images/advertisements/ad3.jpeg', link: 'https://nigex.xyz' },
+        { src: 'https://cdn.pidoras.xyz/assets/images/advertisements/ad4.jpg', link: 'https://nigex.xyz' },
+        { src: 'https://cdn.pidoras.xyz/assets/images/advertisements/ad5.jpeg', link: 'https://nigex.xyz' },
+        { src: 'https://cdn.pidoras.xyz/assets/images/advertisements/ad6.jpg', link: 'https://nigex.xyz' },
+        { src: 'https://cdn.pidoras.xyz/assets/images/advertisements/ad7.png', link: 'https://nigex.xyz/vklog' }
+    ];
 
-document.getElementById('randomImage').src = selectedImage.src;
-document.getElementById('randomImageLink').href = selectedImage.link;
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const selectedImage = images[randomIndex];
 
-// More ads soon
+    const imageElement = document.getElementById('randomImage');
+    const linkElement = document.getElementById('randomImageLink');
+
+    if (imageElement && linkElement) {
+        imageElement.src = selectedImage.src;
+        linkElement.href = selectedImage.link;
+    }
+});
